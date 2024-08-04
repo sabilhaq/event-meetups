28dec43355a3ef419c21dd23fbf37bc38f59975b
Sabil Haq <makrifatsabilhaq@gmail.com>
2024-08-04 11:00:21 +0700

    feat: add event storage mysql

---
+++ internal/driven/storage/mysql/eventstrg/model.go
@@ -0,0 +1,15 @@
+package userstrg
+
+import "github.com/Haraj-backend/hex-monscape/internal/core/entity"
+
+type EventRow struct {
+	ID   int    `db:"id"`
+	Name string `db:"name"`
+}
+
+func (r *EventRow) ToEvent() *entity.Event {
+	return &entity.Event{
+		ID:   r.ID,
+		Name: r.Name,
+	}
+}
---
+++ internal/driven/storage/mysql/eventstrg/storage.go
@@ -0,0 +1,74 @@
+package userstrg
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	"github.com/Haraj-backend/hex-monscape/internal/core/entity"
+	"github.com/Haraj-backend/hex-monscape/internal/driven/storage/mysql/shared"
+	"github.com/jmoiron/sqlx"
+	"gopkg.in/validator.v2"
+)
+
+type Storage struct {
+	sqlClient *sqlx.DB
+}
+
+type Config struct {
+	SQLClient *sqlx.DB `validate:"nonnil"`
+}
+
+func (c Config) Validate() error {
+	return validator.Validate(c)
+}
+
+func New(cfg Config) (*Storage, error) {
+	err := cfg.Validate()
+	if err != nil {
+		return nil, err
+	}
+	s := &Storage{sqlClient: cfg.SQLClient}
+	return s, nil
+}
+
+func (s *Storage) GetEvents(ctx context.Context) ([]entity.Event, error) {
+	var rows shared.EventRows
+	args := []interface{}{}
+	query := `
+		SELECT
+			id,
+			name
+		FROM event
+	`
+
+	if err := s.sqlClient.SelectContext(ctx, &rows, query, args...); err != nil {
+		if err == sql.ErrNoRows {
+			return nil, nil
+		}
+		return nil, fmt.Errorf("unable to execute query due: %w", err)
+	}
+
+	return rows.ToEvents(), nil
+}
+
+func (s *Storage) GetEvent(ctx context.Context, eventID int) (*entity.Event, error) {
+	var event EventRow
+	query := `
+		SELECT
+			id,
+			name
+		FROM event
+		WHERE eventID = ?
+	`
+
+	if err := s.sqlClient.GetContext(ctx, &event, query, eventID); err != nil {
+		if err == sql.ErrNoRows {
+			return nil, nil
+		}
+
+		return nil, fmt.Errorf("unable to find event with id %d: %v", eventID, err)
+	}
+
+	return event.ToEvent(), nil
+}
