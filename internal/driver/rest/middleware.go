21bcb4e324b15e8996936ce1e7caba16ee9b56b8
Sabil Haq <makrifatsabilhaq@gmail.com>
2024-08-04 11:01:24 +0700

    feat: add auth jwt middleware

---
+++ internal/driver/rest/middleware.go
@@ -0,0 +1,61 @@
+package rest
+
+import (
+	"context"
+	"net/http"
+	"strings"
+
+	"github.com/go-chi/render"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var jwtKey = []byte("event-maker")
+
+type Claims struct {
+	UserID int `json:"user_id"`
+	jwt.RegisteredClaims
+}
+
+type contextKey string
+
+const (
+	contextKeyUser = contextKey("user")
+)
+
+func AuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authHeader := r.Header.Get("Authorization")
+		if authHeader == "" {
+			render.Render(w, r, NewErrorResp(NewUnauthorizedError()))
+			return
+		}
+
+		tokenStr := strings.TrimPrefix(authHeader, "Bearer ")
+		claims, err := ValidateJWT(tokenStr)
+		if err != nil {
+			render.Render(w, r, NewErrorResp(NewUnauthorizedError()))
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), contextKeyUser, claims.UserID)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+func ValidateJWT(tokenStr string) (*Claims, error) {
+	claims := &Claims{}
+
+	token, err := jwt.ParseWithClaims(tokenStr, claims, func(token *jwt.Token) (interface{}, error) {
+		return jwtKey, nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if !token.Valid {
+		return nil, jwt.ErrSignatureInvalid
+	}
+
+	return claims, nil
+}
